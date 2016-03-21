788a87c227126a576f6ff877df87517fccdf537c
tmachc <835892182@qq.com>
2016-03-21 21:52:54 +0800

    网络

--- weather/HttpManager.swift
+++ weather/HttpManager.swift
@@ -17,7 +17,44 @@ class HttpManager {
 //        return NetworkReachabilityManager.NetworkReachabilityStatus
 //    }
     
-    func getRequest(url: String, params: [String: AnyObject]?, complete: () -> Void) {
-        
+    func getRequest(
+        url url: String,
+        params: [String: AnyObject]?,
+        complete: (result: [String: AnyObject]!) -> Void)
+    {
+        self.getRequest(url: url, params: params, headers: nil, complete: complete)
+    }
+    
+    func getRequest(
+        url url: String,
+        params: [String: AnyObject]?,
+        headers: [String: String]?,
+        complete: (result: [String: AnyObject]!) -> Void)
+    {
+        var strParam: String = ""
+        if (params != nil) {
+            for (key, value) in params! {
+                strParam += "\(key)=\(value)&"
+            }
+            strParam = strParam.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLHostAllowedCharacterSet())!
+        }
+        print("httpUrl --->>> ",url + strParam)
+        Alamofire.request(.GET, url + strParam, parameters: nil, encoding: .JSON, headers: headers).responseJSON {
+            response in
+            
+//            print("1 --->>> ",response.request)  // original URL request
+//            print("2 --->>> ",response.response) // URL 
+//            print("4 --->>> ",response.result)   // result of response serialization
+            
+            if let JSON = response.result.value {
+                print("result --->>> \(JSON)")
+                complete(result: JSON as! [String : AnyObject])
+            }
+            else {
+                print("访问失败,没有返回值")
+                complete(result: ["errNum": "1", "errMsg": "网络请求失败"])
+            }
+            
+        }
     }
 }
--- weather/ViewController.swift
+++ weather/ViewController.swift
@@ -17,25 +17,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-//        Alamofire.request(
-//            .GET,
-//            "http://apis.baidu.com/apistore/weatherservice/recentweathers?cityname=%E5%8C%97%E4%BA%AC&cityid=101010100",
-//            parameters: nil,
-//            encoding: .JSON,
-//            headers: ["apikey": "ab66b69af4de223187dcc22167846c2e"]).responseJSON { response in
-//                print("1 --->>> ",response.request)  // original URL request
-//                print("2 --->>> ",response.response) // URL response
-//                print("3 --->>> ",response.data)     // server data
-//                print("4 --->>> ",response.result)   // result of response serialization
-//
-//                if let JSON = response.result.value {
-//                    print("JSON: \(JSON)")
-//                    let jsonData = try! NSJSONSerialization.dataWithJSONObject(JSON, options: NSJSONWritingOptions.PrettyPrinted) as NSData!
-//                    let str = NSString.init(data: jsonData, encoding: NSUTF8StringEncoding)!
-//                    self.textView.text = str as String
-//                }
-//        }
-        
+        HttpManager.defaultManager.getRequest(url: "http://apis.baidu.com/apistore/weatherservice/recentweathers?",
+            params: ["cityname": "北京", "cityid": "101010100"],
+            headers: ["apikey": "ab66b69af4de223187dcc22167846c2e"])
+            { (result) -> Void in
+                if result["errNum"]!.isEqual("0") {
+                    let jsonData = try! NSJSONSerialization.dataWithJSONObject(result, options: NSJSONWritingOptions.PrettyPrinted) as NSData!
+                    let str = NSString.init(data: jsonData, encoding: NSUTF8StringEncoding)!
+                    self.textView.text = str as String
+                }
+                else {
+                    
+                }
+        }
         
     }
 
@@ -43,7 +37,5 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
