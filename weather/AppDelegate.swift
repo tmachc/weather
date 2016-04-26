ea3c402ea311dd8f6bdfb563fee44a1de0037e46
tmachc <835892182@qq.com>
2016-04-26 17:31:43 +0800

    修改

--- weather/AppDelegate.swift
+++ weather/AppDelegate.swift
@@ -28,9 +28,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate, WXApiDelegate {
     
     func onReq(req: BaseReq!) {
         //onReq是微信终端向第三方程序发起请求，要求第三方程序响应。第三方程序响应完后必须调用sendRsp返回。在调用sendRsp返回时，会切回到微信终端程序界面。
+        print(req)
     }
     func onResp(resp: BaseResp!) {
         //如果第三方程序向微信发送了sendReq的请求，那么onResp会被回调。sendReq请求调用后，会切到微信终端程序界面。
+        print(resp)
     }
 
     func applicationWillResignActive(application: UIApplication) {
